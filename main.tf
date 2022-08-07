78ec694f2dbfe460c3e8453465db3cc3314ea96f
mnkap <mnkapezov@gmail.com>
2022-08-07 12:37:02 +0600

    add a newline to the end of the file terraform.tfvars variables.tf and separate separate count from the rest of the arguments using a newline

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ resource "aws_vpc" "main" {
 
 resource "aws_subnet" "public" {
   count      = length(var.public_cidr)
+
   vpc_id     = aws_vpc.main.id
   cidr_block = var.public_cidr[count.index]
 
@@ -19,6 +20,7 @@ resource "aws_subnet" "public" {
 
 resource "aws_subnet" "private" {
   count      = length(var.private_cidr)
+
   vpc_id     = aws_vpc.main.id
   cidr_block = var.private_cidr[count.index]
 
@@ -72,6 +74,7 @@ resource "aws_route_table" "public" {
 
 resource "aws_route_table" "private" {
   count  = length(var.private_cidr)
+  
   vpc_id = aws_vpc.main.id
 
   route {
