66b3abb9b4adfbba3b2fa747a51e36a683e43261
Aman <mnkapezov@gmail.com>
2022-09-27 20:43:03 +0600

    Move iam.tf to leve1

--- level1/provider.tf
+++ level1/provider.tf
@@ -18,18 +18,3 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-
-resource "aws_iam_role" "role" {
-  name = "s3fullaccess-role"
-  assume_role_policy =  jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-        "Effect": "Allow",
-        "Action": ["s3:*", "s3-object-lambda:*"],
-        "Resource": "*"
-      }
-  ]
- })  
-
-}
