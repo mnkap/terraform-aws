4e102650289122aa009a73fe9f76e826f05f360f
Aman <mnkapezov@gmail.com>
2022-09-28 18:48:57 +0600

    Add valid name key in the asg.tf and actual ip address in  the instance.tf

--- level2/asg.tf
+++ level2/asg.tf
@@ -4,7 +4,7 @@ resource "aws_launch_configuration" "main" {
   instance_type        = "t2.micro"
   security_groups      = [aws_security_group.private.id]
   user_data            = file("user-data.sh")
-  key_name             = "main"
+  key_name             = "main.pem"
   iam_instance_profile = aws_iam_instance_profile.main.name
 }
 
--- level2/instance.tf
+++ level2/instance.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "public" {
   ami                         = data.aws_ami.amazonlinux.id
   instance_type               = "t3.micro"
   associate_public_ip_address = true
-  key_name                    = "Main"
+  key_name                    = "main.pem"
   vpc_security_group_ids      = [aws_security_group.public.id]
   subnet_id                   = data.terraform_remote_state.level1.outputs.public_subnet_id[1]
 
@@ -22,7 +22,7 @@ resource "aws_security_group" "public" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["212.13.143.195/32"]
+    cidr_blocks = ["185.48.148.185/32"]
   }
 
   ingress {
@@ -30,7 +30,7 @@ resource "aws_security_group" "public" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["212.13.143.195/32"]
+    cidr_blocks = ["185.48.148.185/32"]
   }
 
   egress {
