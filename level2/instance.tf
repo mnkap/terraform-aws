5fae7df4c7846b0e54753663d6e69bdd5ebfa2dd
Aman <mnkapezov@gmail.com>
2022-10-02 12:57:05 +0600

    Delete key main from asg.tf, add manageed_policy_arns in to iam.tf, delete ingress from instance.tf

--- level2/instance.tf
+++ level2/instance.tf
@@ -1,50 +1,3 @@
-#Add resources
-resource "aws_instance" "public" {
-  ami                         = data.aws_ami.amazonlinux.id
-  instance_type               = "t3.micro"
-  associate_public_ip_address = true
-  key_name                    = "main.pem"
-  vpc_security_group_ids      = [aws_security_group.public.id]
-  subnet_id                   = data.terraform_remote_state.level1.outputs.public_subnet_id[1]
-
-  tags = {
-    Name = "${var.env_code}-public"
-  }
-}
-
-resource "aws_security_group" "public" {
-  name        = "${var.env_code}-public"
-  description = "Allow inbound traffic"
-  vpc_id      = data.terraform_remote_state.level1.outputs.vpc_id
-
-  ingress {
-    description = "HTTP from public"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["185.48.148.185/32"]
-  }
-
-  ingress {
-    description = "SSH from public"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["185.48.148.185/32"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "${var.env_code}-public"
-  }
-}
-
 data "aws_ami" "amazonlinux" {
   most_recent = true
 
@@ -71,14 +24,6 @@ resource "aws_security_group" "private" {
   name        = "${var.env_code}-private"
   description = "Allow VPC traffic"
   vpc_id      = data.terraform_remote_state.level1.outputs.vpc_id
-
-  ingress {
-    description = "SSH from VPC"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [data.terraform_remote_state.level1.outputs.vpc_cidr]
-  }
 
   ingress {
     description     = "HTTP from load balancer"
