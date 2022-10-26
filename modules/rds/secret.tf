fb159797eda78992d51942f9c2f161b5fc0b151f
Aman <mnkapezov@gmail.com>
2022-10-26 20:48:22 +0600

    Modified secret.tf

--- modules/rds/secret.tf
+++ modules/rds/secret.tf
@@ -1,9 +1,9 @@
-data "aws_secretsmanager_secret" "by_arn" {
-  arn = "arn:aws:secretsmanager:us-east-1:182678615463:secret:main/rds/password-cE2NPj"
+data "aws_secretsmanager_secret" "rdspassword" {
+  name = "main/rds/password"
 }
 
 data "aws_secretsmanager_secret_version" "rdspassword" {
-  secret_id = data.aws_secretsmanager_secret.by_arn.id
+  secret_id = data.aws_secretsmanager_secret.rdspassword.id
 }
 
 locals {
