02fd4674b58af35e167699e71dc2e9bc26319d3c
Aman <mnkapezov@gmail.com>
2022-11-18 21:20:52 +0600

    Add output.tf and variable to modules/rds

--- level2/output.tf
+++ level2/output.tf
@@ -1,16 +1,4 @@
-output "rdspassword" {
-  value = local.rdspassword
+output "password" {
+  value     = local.rdspassword
   sensitive = true
-}
-
-output "username" {
-  value = module.rds.username
-}
-
-output "db_name" {
-  value = module.rds.db_name
-}
-
-output "instance_class" {
-  value = module.rds.instance_class
-}
+  }
