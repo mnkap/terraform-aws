0730e8d71b89a93492e83ebac8eb65e6247d3608
Aman <mnkapezov@gmail.com>
2022-12-06 21:37:04 +0600

    Add security group

--- level2/rds.tf
+++ level2/rds.tf
@@ -16,6 +16,32 @@ module "rds" {
   create_random_password = false
   username               = "dbadmin"
   password               = local.rdspassword
+  db_subnet_group_name   = aws_db_subnet_group.main.name
   subnet_ids             = [data.terraform_remote_state.level1.outputs.private_subnets[0], data.terraform_remote_state.level1.outputs.private_subnets[1]]
-  vpc_security_group_ids = ["sg-01eb067e815a70acf"]
+  vpc_security_group_ids = [aws_security_group.rds.id]
+}
+
+resource "aws_db_subnet_group" "main" {
+  name       = "main"
+  subnet_ids = data.terraform_remote_state.level1.outputs.private_subnets
+
+  tags = {
+    Name = "Education"
+  }
+}
+
+resource "aws_security_group" "rds" {
+  name   = "education_rds"
+  vpc_id = data.terraform_remote_state.level1.outputs.vpc_id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "education_rds"
+  }
 }
